27743fed364b737fcfc86009589cb61ede8881f5
Jiaji Wu <jiaji.wu@volvocars.com>
2023-05-26 14:04:19 +0200

    chapter 12 walk-through

---
+++ pise5-maven/ch01/src/main/scala/org/ditw/pise5/misc/PackageTest1.scala
@@ -0,0 +1,14 @@
+package org.ditw.pise5.ch12
+
+object PackageTest1 {
+  import Ch12_WR.Lib._
+  // import Ch12_WR.Lib.*
+  val e1 = Elem1(15, "")
+
+  println(e1)
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
